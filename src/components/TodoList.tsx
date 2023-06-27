35dbdd77c48e9886353abf7b661313bfc4bfa85b
PacoLunaMX <f.hinojosa9@hotmail.com>
2023-06-26 19:13:02 -0600

    .

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './styles.css'
 import { Todo } from '../model';
+import SingleTodo from './SingleTodo';
 
 interface Props {
     todos:Todo[];
@@ -11,7 +12,11 @@ interface Props {
 function TodoList({todos, setTodos}:Props) {
   return (
     <div className='todos'>
-
+        {todos.map(todo=>{
+           return( 
+           <SingleTodo todo={todo} key={todo.id} todos={todos} setTodos={setTodos}/>
+            )
+        }) }
     </div>
   )
 }
